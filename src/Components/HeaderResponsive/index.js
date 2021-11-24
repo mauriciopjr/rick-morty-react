d54329075d4ced7a27e5f56836e3762967231db8
mauriciopjr <92741175+mauriciopjr@users.noreply.github.com>
2021-11-23 22:27:58 -0400

    Adicionando rota no link

--- src/Components/HeaderResponsive/index.js
+++ src/Components/HeaderResponsive/index.js
@@ -16,7 +16,7 @@ function HeaderResponsive() {
             </div>
             <ul className={`menu-options${active ? ' active' : '' }`}>
                 <li><a href="/">Home</a></li>
-                <li><a href="/">Personagens</a></li>
+                <li><a href="/personagens">Personagens</a></li>
                 <li><a href="/">Episódios</a></li>
                 <li><a href="/">Sobre</a></li>
             </ul>
